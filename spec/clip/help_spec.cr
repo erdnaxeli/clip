d8812557c12aab82d120d6155e4b405b0d9c3537
Alexandre Morignot <erdnaxeli@cervoi.se>
2021-03-30 13:46:00 +0200

    Test default name in help

--- spec/clip/help_spec.cr
+++ spec/clip/help_spec.cr
@@ -170,6 +170,10 @@ end
 
 describe "Clip::Help" do
   describe ".help" do
+    it "default to PROGRAM_NAME" do
+      EmptyHelp.help.should eq("Usage: #{PROGRAM_NAME}")
+    end
+
     it "show nothing for a empty command" do
       EmptyHelp.help("empty").should eq("Usage: empty")
     end
